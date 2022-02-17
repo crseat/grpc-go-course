f1935b0834cdb63d89ff994907d77a2ea795acd7
Charles Seat <crseat@gmail.com>
2022-02-16 16:17:33 -0800

    initial commit

---
+++ greet/greet_client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"grpc-go-course/greet/greetpb"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	creds := insecure.NewCredentials()
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+
+}
