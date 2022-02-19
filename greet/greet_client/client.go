3860fa42d7768d0e49af05cfef4500a0d6112907
Charles Seat <crseat@gmail.com>
2022-02-19 13:58:51 -0800

    Finish Unary implementation for greet

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"grpc-go-course/greet/greetpb"
 	"log"
@@ -19,6 +20,24 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f", c)
+	// fmt.Printf("Created client: %f", c)
+	doUnary(c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient){
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Charles",
+			LastName:  "Seat",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 
 }
